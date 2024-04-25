9fca509f1f9901dec9667a5da1a8bf7362f7ce7f
naimuddin94 <developernaimuddin@gmail.com>
2024-04-26 01:18:34 +0600

    update user.model.js

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,21 +1,48 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const { sequelize } = require("../db/index");
+const { sequelize } = require("../db");
 
 const User = sequelize.define("User", {
   name: {
     type: DataTypes.STRING,
-    allowNull: false,
+    allowNull: {
+      args: false,
+      msg: "Name is required.",
+    },
   },
   email: {
     type: DataTypes.STRING,
-    allowNull: false,
+    allowNull: {
+      args: false,
+      msg: "Email is required.",
+    },
+    validate: {
+      isEmail: {
+        msg: "Invalid email format",
+      }
+    }
   },
   gender: {
     type: DataTypes.STRING,
+    validate: {
+      isIn: {
+        args: [["male", "female"]],
+        msg: "Gender must be one of: male or female",
+      },
+    },
   },
   mobileNumber: {
     type: DataTypes.STRING,
-    allowNull: false,
+    allowNull: {
+      args: false,
+      msg: "Mobile number is required.",
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: {
+      args: false,
+      msg: "Password is required.",
+    },
   },
 });
 
