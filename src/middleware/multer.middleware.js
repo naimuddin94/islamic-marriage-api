2c2efabe58db53b4ca825b48df30924aa8af6507
naimuddin94 <developernaimuddin@gmail.com>
2024-04-25 23:19:27 +0600

    feat: create multer.middleware.js

---
+++ src/middleware/multer.middleware.js
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination(_req, _file, cb) {
+    cb(null, "./uploads");
+  },
+  filename(req, file, cb) {
+    console.log(10, file);
+    const fileExt = path.extname(file.originalname);
+    const fileName = `${file.originalname
+      .replace(fileExt, "")
+      .toLocaleLowerCase()
+      .split(" ")
+      .join("-")}-${Date.now()}`;
+
+    cb(null, fileName + fileExt);
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = { upload };
