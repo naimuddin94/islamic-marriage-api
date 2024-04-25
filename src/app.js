a6e060e2f9d0749efcfd1128c319e9cfc4ed6abc
naimuddin94 <developernaimuddin@gmail.com>
2024-04-26 03:12:36 +0600

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 /*
- * Title: Islamic Marriage Application 
+ * Title: Islamic Marriage Application
  * Description: A online marriage backend application with express
  * Author: Md Naim Uddin
  * Date: 25/04/2024
@@ -11,6 +11,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { globalErrorHandler } = require("./lib/globalErrorHandler");
+const { userRouter } = require("./routes/user.route");
 
 const app = express();
 
@@ -18,27 +19,25 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:3000"
-    ],
+    origin: ["http://localhost:5173", "http://localhost:3000"],
     credentials: true,
   })
 );
 app.use(cookieParser());
 
 // routes
+app.use("/api/v1/users", userRouter);
 
 // testing route
-app.get('/', (req, res) => {
-    res.send('Server is running....');
+app.get("/", (req, res) => {
+  res.send("Server is running....");
 });
 
 // handling all route which is not found
-app.all('*', (req, res, next) => {
-    const error = new Error(`Can't find ${req.originalUrl} on the server`);
-    error.status = 404;
-    next(error);
+app.all("*", (req, res, next) => {
+  const error = new Error(`Can't find ${req.originalUrl} on the server`);
+  error.status = 404;
+  next(error);
 });
 
 // error handling middleware
