4acae83bff5d9a2c5ae6a115fdbd5fbd3eede729
naimuddin94 <developernaimuddin@gmail.com>
2024-04-26 11:23:38 +0600

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /*
  * Title: Islamic Marriage Application
  * Description: A online marriage backend application with express
@@ -7,37 +8,39 @@
  */
 
 // dependencies
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const { globalErrorHandler } = require("./lib/globalErrorHandler");
-const { userRouter } = require("./routes/user.route");
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { globalErrorHandler } = require('./lib/globalErrorHandler');
+const { userRouter } = require('./routes/user.route');
+const { authRouter } = require('./routes/auth.route');
 
 const app = express();
 
 // middleware
 app.use(express.json());
 app.use(
-  cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
-    credentials: true,
-  })
+    cors({
+        origin: ['http://localhost:5173', 'http://localhost:3000'],
+        credentials: true,
+    }),
 );
 app.use(cookieParser());
 
 // routes
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/auth', authRouter);
 
 // testing route
-app.get("/", (req, res) => {
-  res.send("Server is running....");
+app.get('/', (req, res) => {
+    res.send('Server is running....');
 });
 
 // handling all route which is not found
-app.all("*", (req, res, next) => {
-  const error = new Error(`Can't find ${req.originalUrl} on the server`);
-  error.status = 404;
-  next(error);
+app.all('*', (req, res, next) => {
+    const error = new Error(`Can't find ${req.originalUrl} on the server`);
+    error.status = 404;
+    next(error);
 });
 
 // error handling middleware
