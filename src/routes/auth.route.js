5b2936ca86f27dc52490fbbe1e6d5e9e40d43796
naimuddin94 <developernaimuddin@gmail.com>
2024-04-26 11:54:02 +0600

    update auth.route.js

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
-const { login } = require('../controllers/user.controller');
+const { login, logout } = require('../controllers/user.controller');
+const { verifyToken } = require('../middleware/auth.middleware');
 
 const authRouter = Router();
 
 authRouter.route('/login').post(login);
+authRouter.route('/logout').post(verifyToken, logout);
 
 module.exports = { authRouter };
