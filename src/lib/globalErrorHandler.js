c0297b554f6d726d51ee81d30cd6a147d0800243
naimuddin94 <developernaimuddin@gmail.com>
2024-04-26 11:24:36 +0600

    validator add trim method

--- src/lib/globalErrorHandler.js
+++ src/lib/globalErrorHandler.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 const globalErrorHandler = (err, _req, res, _next) => {
-  // format error
-  res.status(err.status || 500).json({
-    message: err.message,
-    errors: err.errors,
-  });
+    // format error
+    res.status(err.status || 500).json({
+        message: err.message,
+        errors: err.errors,
+    });
 };
 
 module.exports = { globalErrorHandler };
--- src/lib/validators.js
+++ src/lib/validators.js
@@ -1,12 +1,12 @@
 function emptyValidator(body, fieldList) {
-  const errors = [];
-  fieldList.forEach((key) => {
-    if (!body[key]) {
-      errors.push(`${key} is required`);
-    }
-  });
+    const errors = [];
+    fieldList.forEach((key) => {
+        if (!body[key].trim()) {
+            errors.push(`${key} is required`);
+        }
+    });
 
-  return errors;
+    return errors;
 }
 
 module.exports = { emptyValidator };
