24db5c9dd196a0161932558e943224cc7fa82bc0
naimuddin94 <developernaimuddin@gmail.com>
2024-04-29 01:31:06 +0600

    update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,99 +5,108 @@ const jwt = require('jsonwebtoken');
 const { sequelize } = require('../db');
 
 const User = sequelize.define(
-    'User',
-    {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: Sequelize.UUIDV4,
-            primaryKey: true,
-        },
-        fullName: {
-            type: DataTypes.STRING,
-            allowNull: {
-                args: false,
-                msg: 'Name is required.',
-            },
-        },
-        email: {
-            type: DataTypes.STRING,
-            unique: {
-                msg: 'Email already exists',
-            },
-            allowNull: {
-                args: false,
-                msg: 'Email is required.',
-            },
-            validate: {
-                isEmail: {
-                    msg: 'Invalid email format',
-                },
-            },
-        },
-        gender: {
-            type: DataTypes.STRING,
-            validate: {
-                isIn: {
-                    args: [['male', 'female']],
-                    msg: 'Gender must be one of: male or female',
-                },
-            },
-        },
-        mobileNumber: {
-            type: DataTypes.STRING,
-            allowNull: {
-                args: false,
-                msg: 'Mobile number is required.',
-            },
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: {
-                args: false,
-                msg: 'Password is required.',
-            },
-        },
-        refreshToken: {
-            type: DataTypes.STRING,
-        },
-        otp: {
-            type: DataTypes.STRING,
-        },
-        otpExpiry: {
-            type: DataTypes.DATE,
-        },
-        isVerified: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-        },
-        role: {
-            type: DataTypes.STRING,
-            defaultValue: 'basic',
-            validate: {
-                isIn: {
-                    args: [['admin', 'moderator', 'mentor', 'basic']],
-                    msg: 'Role must be one of: admin, moderator, mentor, basic',
-                },
-            },
-        },
+  'User',
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
     },
-    {
-        hooks: {
-            // Before saving the user instance, hash the password if it's changed or new
-            beforeSave: async (user) => {
-                if (user.changed('password')) {
-                    const salt = await bcrypt.genSalt(10);
-                    // eslint-disable-next-line no-param-reassign
-                    user.password = await bcrypt.hash(user.password, salt);
-                }
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Name is required.',
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: {
+        msg: 'Email already exists',
+      },
+      allowNull: {
+        args: false,
+        msg: 'Email is required.',
+      },
+      validate: {
+        isEmail: {
+          msg: 'Invalid email format',
+        },
+      },
+    },
+    gender: {
+      type: DataTypes.STRING,
+      validate: {
+        isIn: {
+          args: [['male', 'female']],
+          msg: 'Gender must be one of: male or female',
+        },
+      },
+    },
+    mobileNumber: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Mobile number is required.',
+      },
+      unique: {
+        msg: 'Email already exists',
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Password is required.',
+      },
+    },
+    refreshToken: {
+      type: DataTypes.STRING,
+    },
+    otp: {
+      type: DataTypes.STRING,
+    },
+    otpExpiry: {
+      type: DataTypes.DATE,
+    },
+    isVerified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      defaultValue: 'basic',
+      validate: {
+        isIn: {
+          args: [['admin', 'moderator', 'mentor', 'basic']],
+          msg: 'Role must be one of: admin, moderator, mentor, basic',
+        },
+      },
+    },
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ['email', 'mobileNumber'],
+      },
+    ],
+    hooks: {
+      // Before saving the user instance, hash the password if it's changed or new
+      beforeSave: async (user) => {
+        if (user.changed('password')) {
+          const salt = await bcrypt.genSalt(10);
+          // eslint-disable-next-line no-param-reassign
+          user.password = await bcrypt.hash(user.password, salt);
+        }
 
-                if (user.changed('email')) {
-                  // eslint-disable-next-line no-param-reassign
-                  user.email = user.email.toLowerCase();
-                }
-            },
-        },
+        if (user.changed('email')) {
+          // eslint-disable-next-line no-param-reassign
+          user.email = user.email.toLowerCase();
+        }
+      },
     },
+  },
 );
 
 // create custom method for password checking
