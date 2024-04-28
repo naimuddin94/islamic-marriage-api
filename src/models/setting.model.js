3c6893b795ee2616867103e5de0703d2b9b5414b
naimuddin94 <developernaimuddin@gmail.com>
2024-04-28 22:08:52 +0600

    feat: create setting.model.js

---
+++ src/models/setting.model.js
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../db');
+
+const Settings = sequelize.define('Settings', {
+  stripePublisherKey: {
+    type: DataTypes.STRING,
+  },
+  stripeSecretKey: {
+    type: DataTypes.STRING,
+  },
+  payPalClientKey: {
+    type: DataTypes.STRING,
+  },
+  zellePayNumber: {
+    type: DataTypes.STRING,
+  },
+  smsApi: {
+    type: DataTypes.STRING,
+  },
+  adminEmailAddress: {
+    type: DataTypes.STRING,
+  },
+  successfulMarriage: {
+    type: DataTypes.STRING,
+  },
+  youtubeEmbedVideoLink: {
+    type: DataTypes.STRING,
+  },
+  logo: {
+    type: DataTypes.STRING,
+  },
+});
+
+module.exports = Settings;
