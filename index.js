aa934380570f72a3c1e7d6f63867544d853a5fb0
naimuddin94 <developernaimuddin@gmail.com>
2024-04-28 22:08:04 +0600

    update index.js

--- index.js
+++ index.js
@@ -7,23 +7,24 @@
  */
 
 // dependencies
-require("dotenv").config();
-const app = require("./src/app");
-const { sequelize, createDatabase } = require("./src/db");
-const http = require("http");
+require('dotenv').config();
+const http = require('http');
+const app = require('./src/app');
+const { sequelize, createDatabase } = require('./src/db');
+
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 (async () => {
-  try {
-    await createDatabase(); 
-    await sequelize.authenticate(); // Authenticate Sequelize
-    console.log("Connection has been established successfully.");
-    await sequelize.sync(); // Sync models
-    server.listen(port, () => {
-      console.log(`🛩️ Server listening on ${port}`);
-    });
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
+    try {
+        await createDatabase();
+        await sequelize.authenticate(); // Authenticate Sequelize
+        console.log('Connection has been established successfully.');
+        await sequelize.sync(); // Sync models
+        server.listen(port, () => {
+            console.log(`🛩️ Server listening on ${port}`);
+        });
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
 })();
