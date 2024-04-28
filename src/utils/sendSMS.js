a56982a8bd15eddf2872ac537e77d38a1abceaf0
naimuddin94 <developernaimuddin@gmail.com>
2024-04-29 01:30:52 +0600

    update sendSMS.js

--- src/utils/sendSMS.js
+++ src/utils/sendSMS.js
@@ -1,23 +1,24 @@
 /* eslint-disable camelcase */
 require('dotenv').config();
 const axios = require('axios');
+const Settings = require('../models/setting.model');
+const { ApiError } = require('./ApiError');
 
 // Function to send SMS using elitbuzz API
 async function sendSMS(message, phoneNumber) {
     try {
-        const api_key = process.env.ELITBUZZ_API_KEY;
-        const senderid = process.env.ELITBUZZ_SENDER_ID;
-        const url = 'https://msg.elitbuzz-bd.com/smsapi';
-
-        const response = await axios.post(url, {
-            api_key,
-            senderid,
+        const setting = await Settings.findByPk(1);
+        if (!setting) {
+            throw new ApiError(404, 'Not found admin sms setting');
+        }
+        const response = await axios.post(setting?.smsApi, {
+            api_key: setting?.smsApiKey,
+            senderid: setting?.smsSenderId,
             type: 'text',
             msg: message,
             contacts: phoneNumber,
         });
 
-        console.log('from sendSMS 17: ', response.data);
         return response.data;
     } catch (error) {
         console.error('Error sending SMS:', error);
