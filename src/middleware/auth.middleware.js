aa52c241bc35cc3ee3b9841c04d9e5ece5e9853b
naimuddin94 <developernaimuddin@gmail.com>
2024-04-28 22:08:48 +0600

    update auth.middleware.js

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,31 +4,45 @@ const User = require('../models/user.model');
 const { ApiError, asyncHandler } = require('../utils');
 
 const verifyToken = asyncHandler(async (req, res, next) => {
-    try {
-        const token = req
-            .cookies?.accessToken || req.header('Authorization')?.replace('Bearer ', '');
+  try {
+    const token = req.cookies?.accessToken
+      || req.header('Authorization')?.replace('Bearer ', '');
 
-        if (!token) {
-            throw new ApiError(401, 'Unauthorized request');
-        }
-
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-        const user = await User.findByPk(decodedToken.id, {
-          attributes: {
-            exclude: ['password', 'createdAt', 'updatedAt', 'mobileNumber', 'gender'],
-          },
-        });
-
-        if (!user) {
-            throw new ApiError(401, 'Invalid access token');
-        }
-
-        req.user = user;
-        next();
-    } catch (error) {
-        throw new ApiError(401, error?.message || 'Invalid access token');
+    if (!token) {
+      throw new ApiError(401, 'Unauthorized request');
     }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findByPk(decodedToken.id, {
+      attributes: {
+        exclude: [
+          'password',
+          'createdAt',
+          'updatedAt',
+          'mobileNumber',
+          'gender',
+        ],
+      },
+    });
+
+    if (!user) {
+      throw new ApiError(401, 'Invalid access token');
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || 'Invalid access token');
+  }
 });
 
-module.exports = { verifyToken };
+const verifyAdmin = asyncHandler(async (req, res, next) => {
+  const isAdmin = req.user.role === 'admin';
+  if (!isAdmin) {
+    throw new ApiError(401, 'Forbidden access');
+  }
+  next();
+});
+
+module.exports = { verifyToken, verifyAdmin };
