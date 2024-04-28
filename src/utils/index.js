f0337db9f8f3627261aed7da40c63a0b5e33f33e
naimuddin94 <developernaimuddin@gmail.com>
2024-04-28 22:09:13 +0600

    update src/utils/index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,8 @@ const { ApiError } = require('./ApiError');
 const { ApiResponse } = require('./ApiResponse');
 const { asyncHandler } = require('./asyncHandler');
 
-module.exports = { ApiError, ApiResponse, asyncHandler };
+module.exports = {
+    ApiError,
+    ApiResponse,
+    asyncHandler,
+};
