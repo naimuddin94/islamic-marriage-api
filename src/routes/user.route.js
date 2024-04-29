cfe8374145857f7a5e043ef080feefebe62ad749
naimuddin94 <developernaimuddin@gmail.com>
2024-04-29 23:52:49 +0600

    change full name

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
-const { registerUser } = require('../controllers/user.controller');
+const { registerUser, changeFullName } = require('../controllers/user.controller');
+const { verifyToken } = require('../middleware/auth.middleware');
 
 const userRouter = Router();
 
 userRouter.route('/register').post(registerUser);
+userRouter.route('/change-fullname').post(verifyToken, changeFullName);
 
 module.exports = { userRouter };
