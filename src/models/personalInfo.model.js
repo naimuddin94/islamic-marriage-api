4963712815ee853c79845b6c76ce4b05f3ab3860
naimuddin94 <developernaimuddin@gmail.com>
2024-05-01 00:33:36 +0600

    create personal info model

---
+++ src/models/personalInfo.model.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../db');
+const User = require('./user.model');
+
+const PersonalInfo = sequelize.define('PersonalInformation', {
+    typeOfBiodata: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    maritalStatus: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    dateOfBirth: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    height: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    complexion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    weight: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    bloodGroup: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    nationality: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+User.hasOne(PersonalInfo, {
+    foreignKey: {
+        allowNull: false,
+    },
+    onDelete: 'cascade',
+});
+PersonalInfo.belongsTo(User);
+
+module.exports = PersonalInfo;
