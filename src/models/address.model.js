e3ab8891dbea5d7e2ff6370b283d0024af0727d9
naimuddin94 <developernaimuddin@gmail.com>
2024-05-01 01:52:21 +0600

    feat: create address.model.js

---
+++ src/models/address.model.js
@@ -0,0 +1,51 @@
+/* eslint-disable prettier/prettier */
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../db');
+const User = require('./user.model');
+
+const Address = sequelize.define(
+  'Address',
+  {
+    permanentAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isSameCurrentAddress: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    currentAddress: {
+      type: DataTypes.STRING,
+        allowNull: false,
+        set(value) {
+            if (this.isSameCurrentAddress) {
+              this.setDataValue('currentAddress', this.permanentAddress);
+            } else {
+              this.setDataValue('currentAddress', value);
+            }
+        },
+    },
+    growUp: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ['UserId'],
+      },
+    ],
+  },
+);
+
+User.hasOne(Address, {
+  foreignKey: {
+    allowNull: false,
+  },
+  onDelete: 'cascade',
+});
+Address.belongsTo(User);
+
+module.exports = Address;
