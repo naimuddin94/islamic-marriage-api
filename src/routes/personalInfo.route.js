da8fb4d299c14275628237702e1a8122b1eb49fc
naimuddin94 <developernaimuddin@gmail.com>
2024-05-01 01:52:34 +0600

    chore: delete personalInfo.route.js

--- src/routes/personalInfo.route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { createPersonalInfo } = require('../controllers/personalInfo.controller');
-const { verifyToken } = require('../middleware/auth.middleware');
-
-const router = Router();
-
-router.route('/personal-info').post(verifyToken, createPersonalInfo);
-
-module.exports = router;
