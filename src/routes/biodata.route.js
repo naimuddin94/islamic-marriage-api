31f3ad5e6976bbc9f492ec1e60b084ec274edbb2
naimuddin94 <developernaimuddin@gmail.com>
2024-05-02 00:38:37 +0600

    update biodata.route.js

--- src/routes/biodata.route.js
+++ src/routes/biodata.route.js
@@ -6,7 +6,9 @@ const {
 const { createAddress, updateAddress } = require('../controllers/address.controller');
 const { createEducation, updateEducation } = require('../controllers/education.controller');
 const { createFamilyInfo, updateFamilyInfo } = require('../controllers/familyInfo.controller');
+const { createLifeStyleInfo, updateLifeStyleInfo } = require('../controllers/lifeStyle.controller');
 const { verifyToken } = require('../middleware/auth.middleware');
+const { upload } = require('../middleware/multer.middleware');
 
 const biodataRouter = Router();
 
@@ -18,5 +20,11 @@ biodataRouter.route('/education/create').post(verifyToken, createEducation);
 biodataRouter.route('/education/update').put(verifyToken, updateEducation);
 biodataRouter.route('/family-info/create').post(verifyToken, createFamilyInfo);
 biodataRouter.route('/family-info/update').put(verifyToken, updateFamilyInfo);
+biodataRouter
+    .route('/lifestyle/create')
+    .post(verifyToken, upload.single('photo'), createLifeStyleInfo);
+biodataRouter
+    .route('/lifestyle/update')
+    .put(verifyToken, upload.single('photo'), updateLifeStyleInfo);
 
 module.exports = biodataRouter;
