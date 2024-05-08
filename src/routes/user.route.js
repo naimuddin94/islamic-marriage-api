c04875d50d269a83743eadeb2785d79fe44b9b45
naimuddin94 <developernaimuddin@gmail.com>
2024-05-08 22:19:42 +0600

    update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,23 @@
 const { Router } = require('express');
-const { registerUser, changeFullName } = require('../controllers/user.controller');
+const {
+    registerUser,
+    changeFullName,
+    visitedBiodata,
+    sortListed,
+    removeIdFromSortList,
+    ignoreList,
+    removeIdFromIgnoreList,
+} = require('../controllers/user.controller');
 const { verifyToken } = require('../middleware/auth.middleware');
 
 const userRouter = Router();
 
 userRouter.route('/register').post(registerUser);
 userRouter.route('/change-fullname').post(verifyToken, changeFullName);
+userRouter.route('/visited').put(verifyToken, visitedBiodata);
+userRouter.route('/sortlisted').post(verifyToken, sortListed);
+userRouter.route('/remove-from-sortlist').delete(verifyToken, removeIdFromSortList);
+userRouter.route('/ignorelist').post(verifyToken, ignoreList);
+userRouter.route('/remove-from-ignorelist').delete(verifyToken, removeIdFromIgnoreList);
 
 module.exports = { userRouter };
