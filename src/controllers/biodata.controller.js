b306c1322b5eb192f519b2697b7d2efef0ae06d8
naimuddin94 <developernaimuddin@gmail.com>
2024-05-09 09:12:50 +0600

    create a controller searched biodata on query

--- src/controllers/biodata.controller.js
+++ src/controllers/biodata.controller.js
@@ -1,6 +1,9 @@
+/* eslint-disable radix */
+/* eslint-disable prettier/prettier */
 /* eslint-disable object-curly-newline */
 /* eslint-disable no-unused-vars */
 /* eslint-disable operator-linebreak */
+const { Op } = require('sequelize');
 const User = require('../models/user.model');
 const PersonalInfo = require('../models/personalInfo.model');
 const Address = require('../models/address.model');
@@ -16,182 +19,333 @@ const { ApiError, ApiResponse, asyncHandler } = require('../utils');
 
 // Get all biodata
 const getAllBiodata = asyncHandler(async (req, res) => {
-    // Fetch all users along with their associated biodata information
-    const usersWithBiodata = await User.findAll({
-        include: [
-            {
-                model: Address,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: PersonalInfo,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: Education,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: FamilyInfo,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: LifeStyle,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: MarriageInfo,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: Occupation,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: ExpectedPartner,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: Pledge,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: Contact,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-        ],
+  const { offset, limit } = req.query;
+
+  // Fetch all users along with their associated biodata information
+  const { count, rows } = await User.findAndCountAll({
+    include: [
+      {
+        model: Address,
         attributes: {
-            exclude: [
-                'password',
-                'refreshToken',
-                'otp',
-                'otpExpiry',
-                'isVerified',
-                'createdAt',
-                'updatedAt',
-            ],
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
         },
-    });
+      },
+      {
+        model: PersonalInfo,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: Education,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: FamilyInfo,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: LifeStyle,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: MarriageInfo,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: Occupation,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: ExpectedPartner,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: Pledge,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: Contact,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+    ],
+    attributes: {
+      exclude: [
+        'password',
+        'refreshToken',
+        'otp',
+        'otpExpiry',
+        'isVerified',
+        'createdAt',
+        'updatedAt',
+      ],
+    },
+    offset: parseInt(offset),
+    limit: parseInt(limit),
+  });
 
-    if (!usersWithBiodata) {
-        throw new ApiError(404, 'No biodata found');
-    }
+  if (!rows) {
+    throw new ApiError(404, 'No biodata found');
+  }
 
-    // Filter out users who are not approved
-    const approvedBiodata = usersWithBiodata.filter((user) => user.isApproved);
+  // Filter out users who are not approved
+  const biodata = rows.filter((user) => user.isApproved);
 
-    return res
-        .status(200)
-        .json(new ApiResponse(200, approvedBiodata, 'Biodata fetched successfully'));
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, { count, biodata }, 'Biodata fetched successfully'),
+    );
 });
 
 // Get single biodata
 const getSingleBiodata = asyncHandler(async (req, res) => {
-    const { userId } = req.params;
-    // Fetch user along with his associated biodata information
-    const userWithBiodata = await User.findByPk(userId, {
-        include: [
-            {
-                model: Address,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: PersonalInfo,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: Education,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: FamilyInfo,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: LifeStyle,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: MarriageInfo,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: Occupation,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: ExpectedPartner,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: Pledge,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-            {
-                model: Contact,
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt', 'UserId'],
-                },
-            },
-        ],
+  const { userId } = req.params;
+  // Fetch user along with his associated biodata information
+  const userWithBiodata = await User.findByPk(userId, {
+    include: [
+      {
+        model: Address,
         attributes: {
-            exclude: [
-                'password',
-                'refreshToken',
-                'otp',
-                'otpExpiry',
-                'isVerified',
-                'createdAt',
-                'updatedAt',
-            ],
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
         },
-    });
+      },
+      {
+        model: PersonalInfo,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: Education,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: FamilyInfo,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: LifeStyle,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: MarriageInfo,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: Occupation,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: ExpectedPartner,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: Pledge,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: Contact,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+    ],
+    attributes: {
+      exclude: [
+        'password',
+        'refreshToken',
+        'otp',
+        'otpExpiry',
+        'isVerified',
+        'createdAt',
+        'updatedAt',
+      ],
+    },
+  });
 
-    if (!userWithBiodata) {
-        throw new ApiError(404, 'No biodata found');
-    }
+  if (!userWithBiodata) {
+    throw new ApiError(404, 'No biodata found');
+  }
 
-    return res
-        .status(200)
-        .json(new ApiResponse(200, userWithBiodata, 'Biodata fetched successfully'));
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, userWithBiodata, 'Biodata fetched successfully'),
+    );
 });
 
-module.exports = { getAllBiodata, getSingleBiodata };
+// Search biodata by criteria
+const searchBiodata = asyncHandler(async (req, res) => {
+  const {
+    offset,
+    limit,
+    permanentAddress,
+    biodataType,
+    maritalStatus,
+    minAge,
+    maxAge,
+  } = req.query;
+
+  // Build query criteria based on provided parameters
+  const queryCriteria = {};
+
+  if (permanentAddress) {
+    queryCriteria['$Address.permanentAddress$'] = permanentAddress;
+  }
+
+  if (biodataType) {
+    queryCriteria['$PersonalInformation.typeOfBiodata$'] = biodataType;
+  }
+
+  if (maritalStatus) {
+    queryCriteria['$PersonalInformation.maritalStatus$'] = maritalStatus;
+  }
+
+  if (minAge && maxAge) {
+    const currentDate = new Date();
+    const minDateOfBirth = new Date(
+      currentDate.getFullYear() - maxAge,
+      currentDate.getMonth(),
+      currentDate.getDate(),
+    );
+    const maxDateOfBirth = new Date(
+      currentDate.getFullYear() - minAge,
+      currentDate.getMonth(),
+      currentDate.getDate(),
+    );
+
+    queryCriteria['$PersonalInformation.dateOfBirth$'] = {
+      [Op.between]: [minDateOfBirth, maxDateOfBirth],
+    };
+  }
+  // Fetch biodata matching the criteria
+  const { count, rows } = await User.findAndCountAll({
+    include: [
+      {
+        model: Address,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: PersonalInfo,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: Education,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: FamilyInfo,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: LifeStyle,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: MarriageInfo,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: Occupation,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: ExpectedPartner,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: Pledge,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+      {
+        model: Contact,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'UserId'],
+        },
+      },
+    ],
+    attributes: {
+      exclude: [
+        'password',
+        'refreshToken',
+        'otp',
+        'otpExpiry',
+        'isVerified',
+        'createdAt',
+        'updatedAt',
+        'sortListed',
+        'ignoreList',
+        'purchased',
+        'connection',
+        'sortListed',
+      ],
+    },
+    where: queryCriteria,
+    offset: parseInt(offset),
+    limit: parseInt(limit),
+  });
+
+  if (!rows) {
+    throw new ApiError(404, 'No biodata found');
+  }
+
+  // Filter out users who are not approved
+  const biodata = rows.filter((user) => user.isApproved);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, { count, biodata }, 'Biodata fetched successfully'),
+    );
+});
+
+module.exports = { getAllBiodata, getSingleBiodata, searchBiodata };
