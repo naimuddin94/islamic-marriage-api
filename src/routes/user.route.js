e3dadc87da707251a44eded10af08545a1e406ae
naimuddin94 <developernaimuddin@gmail.com>
2024-05-11 09:32:16 +0600

    update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -7,6 +7,7 @@ const {
     removeIdFromSortList,
     ignoreList,
     removeIdFromIgnoreList,
+    submitBiodata,
 } = require('../controllers/user.controller');
 const { verifyToken } = require('../middleware/auth.middleware');
 const { searchBiodata } = require('../controllers/biodata.controller');
@@ -21,5 +22,6 @@ userRouter.route('/remove-from-sortlist').delete(verifyToken, removeIdFromSortLi
 userRouter.route('/ignorelist').post(verifyToken, ignoreList);
 userRouter.route('/remove-from-ignorelist').delete(verifyToken, removeIdFromIgnoreList);
 userRouter.route('/searched-biodata').get(searchBiodata);
+userRouter.route('/verify-biodata').post(verifyToken, submitBiodata);
 
 module.exports = { userRouter };
