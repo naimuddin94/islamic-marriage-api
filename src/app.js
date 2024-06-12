dd806f75823dd59f5480b4a9318d00da192b069c
naimuddin94 <developernaimuddin@gmail.com>
2024-06-13 02:24:34 +0600

    update app.js

--- src/app.js
+++ src/app.js
@@ -25,10 +25,14 @@ app.use('/uploads', express.static('uploads'));
 // middleware
 app.use(express.json());
 app.use(
-    cors({
-        origin: ['http://localhost:5173', 'http://localhost:3000'],
-        credentials: true,
-    }),
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://islamicmarriage.vercel.app',
+    ],
+    credentials: true,
+  }),
 );
 app.use(cookieParser());
 
